6a624533215b29450908a67c65d488c8bc53d8c4
VethavarthiniMaha <petchiammal78_bci29@mepcoeng.ac.in>
2026-07-28 16:06:43 +0530

    Create Removal of Duplicates from array.c

---
+++ Removal of Duplicates from array.c
@@ -0,0 +1,12 @@
+int removeDuplicates(int* nums, int numsSize) {
+    if (numsSize == 0) return 0;
+    
+    int k = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (k == 0 || nums[i] != nums[k - 1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+}
