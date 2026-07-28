62b0a675cf62a5b876bba718bc03d0e627d61eef
VethavarthiniMaha <petchiammal78_bci29@mepcoeng.ac.in>
2026-07-28 16:45:29 +0530

    Create Maxium of three numbers using functions.py

---
+++ Maxium of three numbers using functions.py
@@ -0,0 +1,11 @@
+def max_of_three(a,b,c):
+   if a>=b and a>c:
+      return a
+   elif b>=a and b>=c:
+      return b
+   else:
+      return c
+ a=int(input("Enter the first number: "))
+ b=int(input("Enter the second number: "))
+ c=int(input("Enter the third number: "))
+ print( "Maximum Number is",max_of_three(a,b,c))
