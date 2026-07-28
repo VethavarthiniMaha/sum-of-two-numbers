afec8a60c8d28ffdcd5338ed6bc0605d6cf8a16f
VethavarthiniMaha <petchiammal78_bci29@mepcoeng.ac.in>
2026-07-28 16:43:08 +0530

    Create Power of two numbers using functions.py

---
+++ Power of two numbers using functions.py
@@ -0,0 +1,8 @@
+ def power(a,b):
+    result=1
+for i in range(y):
+     result*=x
+    return result
+x=int(input("Enter the base number:"))
+y=int(input("Enter the expo  number:"))
+ print("Power of numbers",power(x,y))
