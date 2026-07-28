b136a26c5bf3ed8e6fac2f57a0bb22e8c2f85113
VethavarthiniMaha <petchiammal78_bci29@mepcoeng.ac.in>
2026-07-28 16:30:00 +0530

    Create Entered number is Positive or negative or zero.py

---
+++ Entered number is Positive or negative or zero.py
@@ -0,0 +1,7 @@
+ a=int(input("Enter the number:"))
+ if a>0:
+   print("The number is Positive number")
+ elif a<0:
+  print("The number is Negative Number")
+  else:
+   print("The number is Zero")
