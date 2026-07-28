b13b9f30cadb49dd05b0c72eb7aa01ed86ade9df
VethavarthiniMaha <petchiammal78_bci29@mepcoeng.ac.in>
2026-07-28 16:25:18 +0530

    Create Average of five subjects.py

---
+++ Average of five subjects.py
@@ -0,0 +1,9 @@
+ m1=int(input("Enter the mark of the first subject:"))
+ m2=int(input("Enter the mark of the second subject:"))
+ m3=int(input("Enter the mark of the third subject:"))
+ m4=int(input("Enter the mark of the fourth subject:"))
+ m5=int(input("Enter the mark of the fifth subjrct:"))
+ Total=m1+m2+m3+m4+m5
+ percentage=(Total/5)
+ print("The Total Marks Obtained by the Student is",Total)
+ print("The Percentage Obtained by the Student is",percentage)
